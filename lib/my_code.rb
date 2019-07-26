23d7c275215ed7d2440c0c206abeb638124fc565
Evan Jeffries <ejeffries14@gmail.com>
2019-07-26 06:18:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,6 +3,12 @@
   dune.map(&block)
  end
  
-
+def reduce(elements, starting_point = nil, &block)
+  if starting_point.nil?
+    elements.reduce(&block)
+  else
+    elements.reduce(starting_point, &block)
+  end
+end
  
  
